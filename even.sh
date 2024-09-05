0ac68150f55ca78c86f50e4e08d6e08d64b42886
arjun <arjunroyal916@gmail.com>
2024-09-05 12:06:26 +0000

    added a even number logic

--- even.sh
+++ even.sh
@@ -1,3 +1,9 @@
 #this topic is all about find even numbers
 read -p "enter a number" NUM
 echo "entered number is : " $NUM
+if [ expr $NUM / 2 = 0 ]
+then
+echo $NUM "is even number"
+else
+	echo $NUM "is not a even number"
+fi
